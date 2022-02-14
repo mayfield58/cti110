0d3a42637a5f89bc5397c7ea1f904470aa6a1212
mayfield58 <99446527+mayfield58@users.noreply.github.com>
2022-02-14 16:19:34 -0500

    Add files via upload

---
+++ P1HW2_MayfieldMichael.py
@@ -0,0 +1,14 @@
+#pizza slice orders
+#14Feb2022
+#CTI-110P1HW2-Pizza Order
+#Michael Mayfield
+#
+Students = int(input('How many students do you want to order pizza for? '))
+Slices = 3
+Pizza = Students * Slices / 6
+print('How many students do you want to order pizza for? : ',(Students))
+print('----Pizza Order--------')
+print('Number of Students : ',(Students))
+print('Pizza Slices Needed: ',(Students * Slices))
+print('Pizzas Needed : ',(Pizza)) 
+print('------------------------')
