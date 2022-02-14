a37a63b9ffe1b443c0fac3c04541c190afca565e
mayfield58 <99446527+mayfield58@users.noreply.github.com>
2022-02-14 15:21:44 -0500

    Add files via upload

---
+++ P1HW1_mayfieldmichael.py
@@ -0,0 +1,8 @@
+num1 = int(input("enter the first number: "))
+num2 = int(input("enter the first number: "))
+sum=num1+num2
+product = num1*num2
+print(num1)
+print(num2)
+print(sum)
+print(product)
