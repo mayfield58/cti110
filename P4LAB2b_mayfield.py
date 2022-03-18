c6b9731b7885db8ad8ade85c6e0e8867b4c4e76e
mayfield58 <99446527+mayfield58@users.noreply.github.com>
2022-03-18 12:02:44 -0400

    Add files via upload

---
+++ P4LAB2b_mayfield.py
@@ -0,0 +1,48 @@
+
+import turtle
+# Initial for MM
+wn = turtle.Screen()
+wn.bgcolor("blue")
+wn.title("Initals")
+michael = turtle.Turtle()
+michael.pensize(15)
+michael.pencolor("white")
+michael.shape("turtle")
+
+#initial M
+michael.penup()
+michael.goto(-100,100)
+michael.pendown()
+michael.right(90)
+michael.forward(150)
+michael.backward(150)
+
+michael.left(45)
+michael.forward(90)
+
+michael.left(95)
+michael.forward(90)
+
+michael.right(140)
+michael.forward(150)
+
+
+
+#intial M
+michael.penup()
+michael.goto(100,50)
+michael.pendown()
+
+michael.forward(90)
+michael.backward(150)
+
+michael.left(45)
+michael.forward(90)
+
+michael.left(95)
+michael.forward(90)
+
+michael.right(140)
+michael.forward(150)
+
+wn.mainloop()
